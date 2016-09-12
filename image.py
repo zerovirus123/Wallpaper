b6b489829e433c48d1a4841d0e971bc41e0480d5
Brian Sia <briansia1@Brians-MacBook-Pro.local>
2016-09-11 18:36:42 -0700

    First commit

---
+++ image.py
@@ -0,0 +1,90 @@
+import pygame
+import random
+
+surface_width = 1400
+surface_height = 700
+LEFT = 0
+RIGHT = 1
+UP = 0
+DOWN = 1
+
+class image(pygame.sprite.Sprite, pygame.Rect):
+
+    def __init__(self,filename, screen):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(filename).convert()
+        self.x_cord = [random.randint(0,surface_width), random.randint(0,2)]
+        self.y_cord = [random.randint(0,surface_height), random.randint(0,2)]
+
+
+    def getImage(self):
+        return self.image
+
+    def getXCord(self):
+        return self.x_cord[0]
+
+    def getYCord(self):
+        return self.y_cord[0]
+
+    def getXDir(self):
+        return self.x_cord[1]
+
+    def getYDir(self):
+        return self.y_cord[1]
+
+    def setXCord(self, x_cord):
+        self.x_cord[0] = x_cord
+
+    def setYCord(self, y_cord):
+        self.x_cord[0] = y_cord
+
+    def setXDir(self, x_dir):
+        self.x_cord[1] = x_dir
+
+    def setYDir(self, y_dir):
+        self.y_cord[1] = y_dir
+
+    def direction(self, maxX, maxY):
+        if self.x_cord[0] < 0:
+           self.x_cord[0] = 0
+           self.x_cord[1] = 1
+
+        if self.x_cord[0] > maxX:
+           self.x_cord[0] = maxX
+           self.x_cord[1] = 0
+
+        if self.getYCord() < 0:
+           self.y_cord[0] = 0
+           self.y_cord[1] = 1
+
+        if self.getYCord() > maxY:
+           self.y_cord[0] = maxY
+           self.y_cord[1] = 0
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x_cord[0], self.y_cord[0]))
+
+
+    def reverseDir(self):
+        if self.x_cord[1] is 1:
+            self.x_cord[1] = 0;
+        else:
+            self.x_cord[1] = 1
+
+        if self.y_cord[1] is 1:
+            self.y_cord[1] = 0
+        else:
+            self.y_cord[1] = 1
+
+    def update(self):
+        if self.x_cord[1] is 1:
+            self.x_cord[0] += 1
+        else:
+            self.x_cord[0] -= 1
+
+        if self.y_cord[1] is 1:
+            self.y_cord[0] += 1
+        else:
+            self.y_cord[0] -= 1
+
+
---
+++ wallpaper.py
@@ -0,0 +1,92 @@
+import pygame
+import sys
+import random
+import image
+import itertools
+
+surface_width = 1400
+surface_height = 720
+background_color =[0,0,255]
+
+screen = pygame.display.set_mode((surface_width, surface_height))
+pygame.display.set_caption("Wtf is this?")
+my_clock = pygame.time.Clock()
+
+def close():
+    for event in pygame.event.get():
+            if event.type is pygame.QUIT:
+                sys.exit()
+            elif event.type is KEYDOWN:
+                sys.exit()
+            else:
+                pass
+close
+
+#if collides with the edge of screen, then changes the direction of the moving logo
+def collision(sprites):
+
+    for pair in itertools.product(sprites, repeat = 2):
+        if pair[0].rect.colliderect(pair[1].rect):
+            for sprite in pair:
+                sprite.reverseDir()
+
+collision
+
+#constantly changes background color after every event tick
+def backgroundColor():
+
+ #   background_color[0] += random.randint(0,10)
+    background_color[1] += random.randint(0,10)
+    background_color[2] += random.randint(0,10)
+
+    if background_color[0] > 255:
+        background_color[0] = 0
+    if background_color[1] > 255:
+        background_color[1] = 0
+    if background_color[2] > 255:
+        background_color[2] = 0
+
+backgroundColor
+
+#moves the logo
+def translate(sprite_list):
+    maxX = 1200
+    maxY = 700
+    close()
+    #first index stores position, second index stores direction
+    for instance in sprite_list:
+        instance.direction(maxX, maxY)
+
+    #determines the direction of motion
+    for instance in sprite_list:
+        instance.update()
+translate
+
+#the main wallpaper event runs in this function, until the user exits it
+def play(sprite_list, sound):
+     while True:
+        counter = 0
+        event = pygame.event.poll()
+        close()
+   #     sound.play()
+        screen.fill(background_color)
+        for sprite in sprite_list:
+           sprite.draw(screen)
+        counter = random.randint(0, 1000000)
+        if counter % 25 == 0:
+            backgroundColor()
+        translate(sprite_list)
+        collision(sprite_list)
+        pygame.display.flip()
+play
+
+#initializes the canvas, the background sound, and the objects
+def main():
+    pygame.init()
+    pygame.mixer.init()
+    sound = pygame.mixer.Sound('rain.wav')
+    tux = image.image('Tux.png')
+    apple = image.image('apple.png')
+    sprite_list = [tux, apple]
+    play(sprite_list, sound)
+main()
